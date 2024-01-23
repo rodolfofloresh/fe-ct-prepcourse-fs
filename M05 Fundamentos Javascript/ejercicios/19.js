07732858ffdc80a5498ee828f1133228aac6817e
unknown <rfloresh92@gmail.com>
2024-01-23 11:28:15 -0500

    avance hasta el ejercicio 19

--- M05 Fundamentos Javascript/ejercicios/19.js
+++ M05 Fundamentos Javascript/ejercicios/19.js
@@ -1,3 +1,4 @@
+
 function esPositivo(num) {
   // La función recibe un entero. Devuelve como resultado un string que indica si el número
   // es positivo o negativo.
@@ -5,6 +6,20 @@ function esPositivo(num) {
   // Si el número es negativo ---> "Es negativo".
   // Si el número es 0, devuelve false.
   // Tu código:
+  if(num > 0) {
+    return("Es positivo");
+  }
+  else if(num < 0) {
+    return("Es negativo");
+  }
+  else(num === 0) 
+    return(false);
+  
 }
+
+console.log("1. es mi consolelog", esPositivo(19));
+console.log("2. es mi consolelog", esPositivo(-18));
+console.log("3. es mi consolelog", esPositivo(0));
+
 
 module.exports = esPositivo;
