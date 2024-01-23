25520ab66cd1cc33a772192e0dedeaed6753e676
unknown <rfloresh92@gmail.com>
2024-01-23 14:24:21 -0500

    Avance hasta el ejercicio 27

--- M05 Fundamentos Javascript/ejercicios/27.js
+++ M05 Fundamentos Javascript/ejercicios/27.js
@@ -2,6 +2,12 @@ function combinarNombres(nombre, apellido) {
   // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
   // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
   // Tu código:
+
+  return ( nombre + " " + apellido);
+  
 }
+
+console.log("(1. Es mi consolelog)", combinarNombres("gool", "perro"));
+console.log("(2. Es mi consolelog)", combinarNombres("boom", "muere"));
 
 module.exports = combinarNombres;
