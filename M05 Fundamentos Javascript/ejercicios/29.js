6a7bfda01d9be32208e697eaad0521d27c941cf1
unknown <rfloresh92@gmail.com>
2024-01-23 14:36:05 -0500

    Avance hasta el ejercicio 29

--- M05 Fundamentos Javascript/ejercicios/29.js
+++ M05 Fundamentos Javascript/ejercicios/29.js
@@ -1,6 +1,8 @@
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el área de un rectángulo teniendo su alto y ancho.
   // Tu código:
+  return (alto * ancho);
 }
 
+console.log(obtenerAreaRectangulo(2, 5));
 module.exports = obtenerAreaRectangulo;
