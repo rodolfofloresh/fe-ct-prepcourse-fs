0787f2b58cf1a746e4739314795c5482c829a046
unknown <rfloresh92@gmail.com>
2024-01-23 13:35:55 -0500

    Avance hasta el ejercicio 24

--- M05 Fundamentos Javascript/ejercicios/24.js
+++ M05 Fundamentos Javascript/ejercicios/24.js
@@ -2,6 +2,9 @@ function divide(x, y) {
   // La función recibe dos argumentos "x" e "y" que son números.
   // Retorna el resultado de su división.
   // Tu código:
+  return (x / y);
 }
 
+console.log("(1. Es mi consolelog)", divide(12, 3));
+console.log("(2. Es mi consolelog)", divide(8, -2));
 module.exports = divide;
