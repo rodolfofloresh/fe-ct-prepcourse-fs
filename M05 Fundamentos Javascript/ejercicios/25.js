c9090b94304cfec385a1a58e50136caf2b72a9be
unknown <rfloresh92@gmail.com>
2024-01-23 13:47:48 -0500

    Avance hasta el ejercicio25

--- M05 Fundamentos Javascript/ejercicios/25.js
+++ M05 Fundamentos Javascript/ejercicios/25.js
@@ -2,6 +2,10 @@ function obtenerResto(x, y) {
   // La función recibe dos argumentos "x" e "y" que son números.
   // Obten el resto de la división de "x" entre "y".
   // Tu código:
+  return (x % y);
+
 }
 
+console.log("(1. Es mi consolelog)", obtenerResto(5, 5));
+console.log("(2. Es mi consolelog)", obtenerResto(15, 4));
 module.exports = obtenerResto;
