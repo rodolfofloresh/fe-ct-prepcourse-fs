9c88cb7bc731d404072997517e1699870e79e28a
unknown <rfloresh92@gmail.com>
2024-01-23 10:43:09 -0500

    avance hasta el ejercicio 18

--- M05 Fundamentos Javascript/ejercicios/17.js
+++ M05 Fundamentos Javascript/ejercicios/17.js
@@ -2,6 +2,10 @@ function esPar(num) {
   // Retorna true si "num" es par.
   // De lo contrario, retorna false.
   // Tu código:
+  return(num % 2 === 0);
 }
+
+console.log("1. es mi consolelog", esPar(10));
+console.log("2. es mi consolelog", esPar(11));
 
 module.exports = esPar;
--- M05 Fundamentos Javascript/ejercicios/18.js
+++ M05 Fundamentos Javascript/ejercicios/18.js
@@ -2,6 +2,10 @@ function esImpar(num) {
   // Retorna true si "num" es impar.
   // De lo contrario, retorna false.
   // Tu código:
+  return(num % 2 === 1);
 }
+
+console.log("1. es mi consolelog", esImpar(11));
+console.log("2. es mi consolelog", esImpar(18));
 
 module.exports = esImpar;
