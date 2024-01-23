8fbbf003ffe5b6899f8526726a89c6188ec754e1
unknown <rfloresh92@gmail.com>
2024-01-23 14:33:28 -0500

    Avance hasta el ejercicio 28

--- M05 Fundamentos Javascript/ejercicios/28.js
+++ M05 Fundamentos Javascript/ejercicios/28.js
@@ -2,6 +2,10 @@ function obtenerSaludo(nombre) {
   // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
   // Ejemplo: "Martin" ---> "Hola Martin!"
   // Tu código:
+  
+  return("Hola" + " " + nombre + "!");
 }
 
+console.log("(1. Es mi consolelog)", obtenerSaludo("Rodolfo"));
+console.log("(2. Es mi consolelog)", obtenerSaludo("Lucero"));
 module.exports = obtenerSaludo;
