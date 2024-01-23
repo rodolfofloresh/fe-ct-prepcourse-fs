6f6d39fc898bdbc6c8031ba3ec706e5f029b86b3
unknown <rfloresh92@gmail.com>
2024-01-23 13:57:06 -0500

    Avance hasta el ejercicio 26

--- M05 Fundamentos Javascript/ejercicios/26.js
+++ M05 Fundamentos Javascript/ejercicios/26.js
@@ -2,6 +2,11 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final del string "str" y retórnalo.
   // Ejemplo: "hello world" ---> "hello world!"
   // Tu código:
+  return ( str + "!");
+
 }
+
+console.log("(1. Es mi consolelog)", agregarSimboloExclamacion("gool"));
+console.log("(2. Es mi consolelog)", agregarSimboloExclamacion("boom"));
 
 module.exports = agregarSimboloExclamacion;
