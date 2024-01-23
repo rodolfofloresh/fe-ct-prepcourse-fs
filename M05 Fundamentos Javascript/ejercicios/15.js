31c31fd4e38c8262dc0a3548a28834c7e79442df
unknown <rfloresh92@gmail.com>
2024-01-23 10:30:36 -0500

    avance hasta el ejercicio 16

--- M05 Fundamentos Javascript/ejercicios/15.js
+++ M05 Fundamentos Javascript/ejercicios/15.js
@@ -2,6 +2,10 @@ function menosQueNoventa(num) {
   // Retorna true si el argumento "num" es menor que noventa.
   // De lo contrario, retorna false.
   // Tu código:
+  return (num < 90);
 }
+
+console.log("1. es mi consolelog", menosQueNoventa(95));
+console.log("2. es mi consolelog", menosQueNoventa(88));
 
 module.exports = menosQueNoventa;
--- M05 Fundamentos Javascript/ejercicios/16.js
+++ M05 Fundamentos Javascript/ejercicios/16.js
@@ -2,6 +2,10 @@ function mayorQueCincuenta(num) {
   // Retorna true si el argumento "num" es mayor que cincuenta.
   // De lo contrario, retorna false.
   // Tu código:
+  return(num > 50);
 }
+
+console.log("1. es mi consolelog", mayorQueCincuenta(52));
+console.log("2. es mi consolelog", mayorQueCincuenta(48));
 
 module.exports = mayorQueCincuenta;
