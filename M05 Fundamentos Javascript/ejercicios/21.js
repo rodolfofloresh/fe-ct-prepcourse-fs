5d94c681c2e354be9a9464169310fedd922a9eb6
unknown <rfloresh92@gmail.com>
2024-01-23 12:55:39 -0500

    avance hasta el ejercicio 21

--- M05 Fundamentos Javascript/ejercicios/21.js
+++ M05 Fundamentos Javascript/ejercicios/21.js
@@ -2,6 +2,11 @@ function suma(x, y) {
   // La función recibe dos argumentos "x" e "y" que son números.
   // Retorna el resultado de su suma.
   // Tu código:
+  return (x + y);
 }
+
+var resultado = suma(7, 2);
+console.log("(1. Es mi console.log)", (resultado));
+
 
 module.exports = suma;
