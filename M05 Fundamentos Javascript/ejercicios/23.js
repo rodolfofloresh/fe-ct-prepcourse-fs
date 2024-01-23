6ca0c22dfbd98569ca3c2d2c0df904e16aee8fd7
unknown <rfloresh92@gmail.com>
2024-01-23 13:12:35 -0500

    avance hasta el ejercicio 23

--- M05 Fundamentos Javascript/ejercicios/23.js
+++ M05 Fundamentos Javascript/ejercicios/23.js
@@ -2,6 +2,12 @@ function multiplica(x, y) {
   // La función recibe dos argumentos "x" e "y" que son números.
   // Retorna el resultado de su multiplicación.
   // Tu código:
+  return (x * y);
 }
+
+console.log("(1. Es mi consolelog)", multiplica(4, 3));
+console.log("(2. Es mi consolelog)", multiplica(4, -3));
+
+
 
 module.exports = multiplica;
