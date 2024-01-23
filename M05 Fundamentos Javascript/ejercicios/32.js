c44a4586d846f9fa371eb854674eac58ff19f5b5
unknown <rfloresh92@gmail.com>
2024-01-23 18:54:09 -0500

    Avance hasta el ejercicio 32

--- M05 Fundamentos Javascript/ejercicios/32.js
+++ M05 Fundamentos Javascript/ejercicios/32.js
@@ -2,6 +2,10 @@ function deEuroAdolar(euro) {
   // Debes calcular el valor recibido como argumento pasándolo a dólares.
   // Supongamos que 1 euro equivale a 1.20 dólares.
   // Tu código:
+  return(euro * 1.20);
 }
+
+console.log("(1. Es mi consolelog)", deEuroAdolar(10));
+console.log("(2. Es mi consolelog)", deEuroAdolar(20));
 
 module.exports = deEuroAdolar;
