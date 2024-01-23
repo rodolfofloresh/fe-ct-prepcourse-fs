32532b48fd3fe8a2fba9d34902821b6f63f3585d
unknown <rfloresh92@gmail.com>
2024-01-23 18:50:35 -0500

    Avance hasta el ejercicio 31

--- M05 Fundamentos Javascript/ejercicios/31.js
+++ M05 Fundamentos Javascript/ejercicios/31.js
@@ -1,6 +1,11 @@
 function areaDelTriangulo(base, altura) {
   // Calcula el área de un triángulo y retorna el resultado.
   // Tu código:
+  return ((base * altura)/2);
 }
+
+console.log("(1. Es mi consolelog)", areaDelTriangulo(5, 10));
+console.log("(2. Es mi consolelog)", areaDelTriangulo(2, 5));
+
 
 module.exports = areaDelTriangulo;
