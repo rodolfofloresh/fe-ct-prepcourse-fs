69645af9de36cd283d0c054bac537eee3e1386f4
unknown <rfloresh92@gmail.com>
2024-01-23 14:46:29 -0500

    Avance hasta el ejercicio 30

--- M05 Fundamentos Javascript/ejercicios/30.js
+++ M05 Fundamentos Javascript/ejercicios/30.js
@@ -2,6 +2,8 @@ function retornarPerimetro(lado) {
    // La función recibe como argumento la medida de un lado de un cuadrado.
    // Debes retornar su perímetro.
    // Tu código:
+   return (lado * 4);
 }
 
+console.log(retornarPerimetro(5));
 module.exports = retornarPerimetro;
