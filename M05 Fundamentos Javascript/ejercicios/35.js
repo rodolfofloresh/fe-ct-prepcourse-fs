ee1a292a56eeeb68060a48bcc97a2a95d8ff4262
unknown <rfloresh92@gmail.com>
2024-01-23 19:41:46 -0500

    Avance hasta el ejercicio 35

--- M05 Fundamentos Javascript/ejercicios/35.js
+++ M05 Fundamentos Javascript/ejercicios/35.js
@@ -3,6 +3,10 @@
 function elevarAlCubo(num) {
   // Retorna el valor de "num" elevado al cubo.
   // Tu código:
+  return (num ** 3);
 }
+
+console.log("(1. Es mi consolelog)", elevarAlCubo(2));
+console.log("(2. Es mi consolelog)", elevarAlCubo(3));
 
 module.exports = elevarAlCubo;
