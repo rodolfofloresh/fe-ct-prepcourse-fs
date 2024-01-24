5aa2a5b3ef1233cf94c054b8dd67782eca2c42b3
unknown <rfloresh92@gmail.com>
2024-01-23 19:57:52 -0500

    ejercicios terminados

--- M05 Fundamentos Javascript/ejercicios/39.js
+++ M05 Fundamentos Javascript/ejercicios/39.js
@@ -3,6 +3,12 @@
 function numeroRandom() {
   // Genera un número al azar entre 0 y 1 y retórnalo.
   // Tu código:
+
+  return Math.random();
 }
+
+console.log(numeroRandom());
+console.log(numeroRandom());
+console.log(numeroRandom());
 
 module.exports = numeroRandom;
