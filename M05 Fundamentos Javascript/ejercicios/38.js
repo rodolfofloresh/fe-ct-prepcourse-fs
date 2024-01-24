3ce01babcd68751700b4536c90dc5e4d79394925
unknown <rfloresh92@gmail.com>
2024-01-23 19:55:02 -0500

    Avance hasta el ejercicio 38

--- M05 Fundamentos Javascript/ejercicios/38.js
+++ M05 Fundamentos Javascript/ejercicios/38.js
@@ -3,6 +3,10 @@
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba y retórnalo.
   // Tu código:
+  return Math.ceil(num);
 }
+
+console.log(redondearHaciaArriba(6.3));
+console.log(redondearHaciaArriba(6.7));
 
 module.exports = redondearHaciaArriba;
