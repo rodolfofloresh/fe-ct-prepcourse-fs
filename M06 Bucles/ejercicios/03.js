a5c0756edf44289075301fdc41429373b20d551c
unknown <rfloresh92@gmail.com>
2024-01-24 19:35:10 -0500

    Desarrollo bucles 03

--- M06 Bucles/ejercicios/03.js
+++ M06 Bucles/ejercicios/03.js
@@ -3,6 +3,18 @@ function obtenerMayor(x, y) {
   // Retorna el número más grande.
   // Si son iguales, retornar cualquiera de los dos.
   // Tu código:
+  if (x < y ) {
+    return y;
+  }
+  else if( x > y) {
+    return x;
+  }
+  else (x === y) 
+    return x || y;
+
 }
+
+console.log("(1. Es mi consolelog)", obtenerMayor(2, 2));
+console.log("(2. Es mi consolelog)", obtenerMayor(3, -5));
 
 module.exports = obtenerMayor;
