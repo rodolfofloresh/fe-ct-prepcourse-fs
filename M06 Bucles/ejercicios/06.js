846fe9e7669ed8b92c54bd0a9ffc0d34104100c3
unknown <rfloresh92@gmail.com>
2024-01-24 21:31:32 -0500

    Desarrollo bucles 06

--- M06 Bucles/ejercicios/06.js
+++ M06 Bucles/ejercicios/06.js
@@ -2,6 +2,12 @@ function tieneTresDigitos(num) {
   // Si el número recibido tiene tres dígitos, retorna true.
   // Caso contrario, retorna false.
   // Tu código:
+  return (num / 100 >=1 && num / 100 <=9.99);
+  
 }
+console.log("(1. es mi consolelog)", tieneTresDigitos(999));
+console.log("(2. es mi consolelog)", tieneTresDigitos(120));
+console.log("(3. es mi consolelog)", tieneTresDigitos(99));
+
 
 module.exports = tieneTresDigitos;
