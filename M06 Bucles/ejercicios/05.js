1eb8c9d93e62a3ff0238952b409847677ba8ea7f
unknown <rfloresh92@gmail.com>
2024-01-24 20:44:34 -0500

    Desarrollo bucle 05

--- M06 Bucles/ejercicios/05.js
+++ M06 Bucles/ejercicios/05.js
@@ -2,6 +2,17 @@ function esVerdadero(valor) {
   // Si "valor" es verdadero, retorna "Soy verdadero".
   // Caso contrario, retorna "Soy falso".
   // Tu código:
+  if (valor === true) {
+    return "Soy verdadero";
+  }
+  else {
+    return "Soy falso";
+  }
+
+
 }
+console.log("(1. Es mi consolelog)", esVerdadero(false));
+console.log("(2. Es mi consolelog)", esVerdadero(true));
+
 
 module.exports = esVerdadero;
