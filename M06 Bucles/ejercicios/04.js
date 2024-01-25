5b619e6a1e310d2bc38c9ce1988650a51097036a
unknown <rfloresh92@gmail.com>
2024-01-24 19:42:34 -0500

    Desarrollo bucles 04

--- M06 Bucles/ejercicios/04.js
+++ M06 Bucles/ejercicios/04.js
@@ -3,6 +3,17 @@ function mayoriaDeEdad(edad) {
   // Si tiene 18 años o más, retorna el string: "Allowed".
   // Caso contrario: "Not allowed".
   // Tu código:
+
+  if (edad >= 18) {
+    return "Allowed";
+  }
+  else {
+    return "Not allowed";
+  }
+
 }
+console.log("(1. Es mi consolelog)", mayoriaDeEdad(17));
+console.log("(2. Es mi consolelog)", mayoriaDeEdad(19));
+
 
 module.exports = mayoriaDeEdad;
