d9124ec841dfa63fb735ea341010d70d4960471a
unknown <rfloresh92@gmail.com>
2024-01-24 19:23:11 -0500

    Desarrollo bucles 02

--- M06 Bucles/ejercicios/02.js
+++ M06 Bucles/ejercicios/02.js
@@ -2,6 +2,17 @@ function esVerdaderoYFalso(booleano1, booleano2) {
   // La función recibe dos argumentos "booleano1" y "booleano2".
   // Retorna true si ambos son verdaderos, caso contrario, retorna false.
   // Tu código:
+  if (booleano1 === true && booleano2 === true) {
+    return true;
+  }
+
+  else {
+    return false;
+  }
+
 }
+
+console.log("(1. Es mi consolelog)", esVerdaderoYFalso(true, true));
+console.log("(2. Es mi consolelog)", esVerdaderoYFalso(false, false));
 
 module.exports = esVerdaderoYFalso;
