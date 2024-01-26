8fb7ae4220c4dab2044c8f0aec907fa68dfa0640
unknown <rfloresh92@gmail.com>
2024-01-25 19:40:34 -0500

    bucle 19

--- M06 Bucles/ejercicios/18.js
+++ M06 Bucles/ejercicios/18.js
@@ -2,6 +2,21 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+  var num = 1;
+  if(a<0){
+    return 0;
+    
+  }
+  for(var i = a ; i <= b ; i++) {
+    num *= i;
+  }
+
+  return num;
+ 
+  
+
 }
+
+console.log(productoEntreNúmeros(2, 5));
 
 module.exports = productoEntreNúmeros;
--- M06 Bucles/ejercicios/19.js
+++ M06 Bucles/ejercicios/19.js
@@ -2,6 +2,14 @@ function sumarHastaN(n) {
   // La función recibe un número "n" por argumento.
   // Devuelve la suma de todos los números desde 1 hasta n.
   // Tu código:
+  var num = 0;
+  for(var i = 1 ; i <= n ; i++){
+    num +=i;
+  } 
+  
+  return num;
+  
 }
+console.log(sumarHastaN(4));
 
 module.exports = sumarHastaN;
