c61fcc4b5fb9ba66934ac00c5c5fa1b0ccc77735
unknown <rfloresh92@gmail.com>
2024-01-25 23:54:18 -0500

    Bucles 21

--- M06 Bucles/ejercicios/21.js
+++ M06 Bucles/ejercicios/21.js
@@ -4,6 +4,18 @@ function esPotenciaDeDos(numero) {
   // Devuelve true si lo es, sino devuelve false.
   // PISTA: Utiliza un bucle while.
   // Tu código:
+  if(numero < 0) {
+    return false;
+  }
+
+  while( numero % 2 === 0){
+    numero /= 2;
+  }
+  return numero === 1;
 }
+
+console.log(esPotenciaDeDos(16));
+console.log(esPotenciaDeDos(18));
+console.log(esPotenciaDeDos(32));
 
 module.exports = esPotenciaDeDos;
