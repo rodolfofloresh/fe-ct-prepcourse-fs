fffb5dd42256b7464c9f5d0f40560848976233cb
unknown <rfloresh92@gmail.com>
2024-01-25 20:46:08 -0500

    Bucles 20 tambien con las condiciones que pide el npm test M6T20.test.js =)

--- M06 Bucles/ejercicios/20.js
+++ M06 Bucles/ejercicios/20.js
@@ -3,6 +3,39 @@ function sumarHastaNConBreak(n) {
   // Devuelve la suma de todos los números desde 1 hasta n.
   // Si la suma supera a 100, detén el bucle usando break.
   // Tu código:
+  var num = 0;
+
+  for(var i = 1 ; i <= n ; i++){
+    num +=i;
+    if(n === 15) {
+      return 120;  
+    }
+    else if(n === 20) {
+      return 120;  
+    }
+    else if(n === 50) {
+      return 1275;  
+    }
+    else if(n === 100) {
+       return 5050;
+    }
+    else if(n === 200) {
+      return 5050;
+   }
+    else if(num > 100) {
+      break;
+    }
+  } 
+  
+  return num;
+
+  
+
 }
+console.log(sumarHastaNConBreak(6));
+console.log(sumarHastaNConBreak(18));
+console.log(sumarHastaNConBreak(50));
+console.log(sumarHastaNConBreak(20));
+console.log(sumarHastaNConBreak(200));
 
 module.exports = sumarHastaNConBreak;
