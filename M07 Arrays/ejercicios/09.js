8b3a567f06962a0ca10a1969df07b2133dbf0213
unknown <rfloresh92@gmail.com>
2024-01-31 14:19:31 -0500

    Arrays 09

--- M07 Arrays/ejercicios/09.js
+++ M07 Arrays/ejercicios/09.js
@@ -1,6 +1,20 @@
 function contarElementosMayoresA10(array) {
   // Cuenta y devuelve la cantidad de elementos mayores a 10 en el array de números.
   // Tu código:
+  var mayor = array.filter(function(elemento) {
+    return elemento > 10;
+  })
+  return mayor.length;
+  
+  
+
+
 }
+
+var numeros = [2, 31, 18, 6];
+var numermayores = contarElementosMayoresA10(numeros);
+
+console.log(numermayores);
+
 
 module.exports = contarElementosMayoresA10;
