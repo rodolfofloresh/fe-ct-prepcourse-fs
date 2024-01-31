7fbf4353160de537461e49c11e9de555b156b148
unknown <rfloresh92@gmail.com>
2024-01-31 17:52:25 -0500

    Arrays 11

--- M07 Arrays/ejercicios/11.js
+++ M07 Arrays/ejercicios/11.js
@@ -2,6 +2,21 @@ function multiplicarElementosPorIndice(array) {
   // Multiplica cada elemento del array por su índice.
   // Devuelve el nuevo arreglo con los resultados.
   // Tu código:
+  var multiplicacion = array.map(function(elemento, indice){
+    return elemento * indice;
+  })
+
+  return multiplicacion
+
 }
+
+var miarray = [2, 5, 3, 1];
+var multiplicardatos = multiplicarElementosPorIndice(miarray);
+
+
+console.log(miarray);
+console.log(multiplicardatos);
+
+
 
 module.exports = multiplicarElementosPorIndice;
