f1a9a2c2a8961957546a889882f4d5aa0cbd50e1
unknown <rfloresh92@gmail.com>
2024-01-30 21:07:11 -0500

    Arrays 02

--- M07 Arrays/ejercicios/01.js
+++ M07 Arrays/ejercicios/01.js
@@ -1,6 +1,17 @@
 function invertirArray(array) {
   // Invierte el arreglo array recibido por argumento.
   // Tu código:
+
+  
+  var invertido = array.reverse();
+  return invertido
+ 
+  
+
+
 }
+var miarray = [1, 2, 3];
+console.log("Es mi console.log:", invertirArray(miarray));
+
 
 module.exports = invertirArray;
--- M07 Arrays/ejercicios/02.js
+++ M07 Arrays/ejercicios/02.js
@@ -2,6 +2,11 @@ function ordenarArray(array) {
   // Ordena los elementos del areglo array de menor a mayor.
   // Devuelve el arreglo resultante.
   // Tu código:
+  var ordenar = array.sort();
+  return ordenar;
+
 }
 
+var miorden = [4, 2, 3, 1];
+console.log("Es mi consolelog:", ordenarArray(miorden));
 module.exports = ordenarArray;
