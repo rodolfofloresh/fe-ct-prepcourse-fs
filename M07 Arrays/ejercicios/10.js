b88a60e1c27265616c6fcdef214edb56f9986c4e
unknown <rfloresh92@gmail.com>
2024-01-31 17:27:48 -0500

    Arrays 10

--- M07 Arrays/ejercicios/10.js
+++ M07 Arrays/ejercicios/10.js
@@ -2,6 +2,20 @@ function encontrarIndiceMayor(array) {
   // Encuentra el índice del número más grande en el array de números.
   // Devuelve el valor de este índice.
   // Tu código:
+  var indicemayor = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] > array[indicemayor]) {
+      indicemayor = i;
+    }
+  }
+
+  return indicemayor;
 }
+
+var valores = [2, 6, 3, 9];
+var numeromaximo = encontrarIndiceMayor(valores);
+
+console.log(numeromaximo);
 
 module.exports = encontrarIndiceMayor;
