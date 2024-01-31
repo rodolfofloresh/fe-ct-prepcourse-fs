b3ca9bd91cec25a3ba3a08ae3c436b16f0482f10
unknown <rfloresh92@gmail.com>
2024-01-31 13:18:59 -0500

    Arrays 07

--- M07 Arrays/ejercicios/07.js
+++ M07 Arrays/ejercicios/07.js
@@ -2,6 +2,22 @@ function convertirStringAMayusculas(array) {
   // Convierte a mayúsculas todos los strings del array.
   // Retorna el arreglo resultante.
   // Tu código:
+  var mayuscula = array.map(function(elemento) {
+    if(typeof elemento === "string") {
+      return elemento.toUpperCase();
+    }
+    return elemento;
+  });
+  return mayuscula;
+
+  
 }
+
+ 
+var array1 = ["perro", "gato", "cocodrilo"];
+var convertir = convertirStringAMayusculas(array1);
+
+
+console.log(convertir);
 
 module.exports = convertirStringAMayusculas;
