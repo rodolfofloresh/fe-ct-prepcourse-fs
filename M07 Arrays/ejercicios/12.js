776c6e16df4fa20742a8db66022e43b34f8f05a0
unknown <rfloresh92@gmail.com>
2024-01-31 18:17:39 -0500

    Arrays 12

--- M07 Arrays/ejercicios/12.js
+++ M07 Arrays/ejercicios/12.js
@@ -1,6 +1,22 @@
 function agregarNumeros(arrayOfNums) {
   // Suma todos los elementos de arrayOfNums y retorna el resultado.
   // Tu código:
+  var suma = 0;
+
+for(let i = 0; i < arrayOfNums.length; i++) {
+  suma = suma + arrayOfNums[i];
+
+} 
+
+return suma;
 }
+
+var miarray = [5, 2, 3];
+var sumaslementos = agregarNumeros(miarray);
+
+console.log(sumaslementos);
+
+
+
 
 module.exports = agregarNumeros;
