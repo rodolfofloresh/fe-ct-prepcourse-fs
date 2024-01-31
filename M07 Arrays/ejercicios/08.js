1a300b26f24575e4e35ff54472c54d245c033afd
unknown <rfloresh92@gmail.com>
2024-01-31 13:44:53 -0500

    Arrays 08

--- M07 Arrays/ejercicios/08.js
+++ M07 Arrays/ejercicios/08.js
@@ -1,6 +1,18 @@
 function filtrarNumerosPares(array) {
   // Devuelve un arreglo solo con los números pares presentes en el array.
   // Tu código:
+  var espar = array.filter(function(elemento) {
+    return elemento % 2 ===0;
+  
+  })
+ 
+  return espar;
+
 }
+
+var numeros = [4, 5, 6, 7, 8, 9];
+var numpar = filtrarNumerosPares(numeros);
+console.log(numpar);
+
 
 module.exports = filtrarNumerosPares;
