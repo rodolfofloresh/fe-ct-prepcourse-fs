6d8216cef4891b3c820048b519aa343f50d4cad7
unknown <rfloresh92@gmail.com>
2024-01-31 19:05:37 -0500

    Arrays 13

--- M07 Arrays/ejercicios/13.js
+++ M07 Arrays/ejercicios/13.js
@@ -1,6 +1,24 @@
 function promedioResultadosTest(resultadosTest) {
   // Itera sobre los elementos del arreglo resultadosTest y devuelve el promedio de las notas.
   // Tu código:
+  var suma = 0;
+  
+  for (let i = 0; i < resultadosTest.length; i++) {
+    suma = suma + resultadosTest[i];
+
+  }
+
+  var promedio = suma / resultadosTest.length;
+  return promedio;
+    
 }
+
+var miarray = [10, 20, 30];
+var mipromedio = promedioResultadosTest(miarray);
+
+console.log(mipromedio);
+
+
+
 
 module.exports = promedioResultadosTest;
