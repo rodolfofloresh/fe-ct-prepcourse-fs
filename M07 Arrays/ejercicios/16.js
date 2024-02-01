2365337877577c6f09ba76a80f9816a151b99585
unknown <rfloresh92@gmail.com>
2024-01-31 21:42:05 -0500

    Arrays 16

--- M07 Arrays/ejercicios/16.js
+++ M07 Arrays/ejercicios/16.js
@@ -3,6 +3,24 @@ function mesesDelAño(array) {
   // "Marzo" y "Noviembre", guardarlos en un nuevo arreglo y retornarlo.
   // Si alguno de los meses no está, retornar el string: "No se encontraron los meses pedidos".
   // Tu código:
+  var mesespedidos = [];
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre") {
+      mesespedidos.push(array[i]);
+    }
+  }
+
+  if(mesespedidos.length === 3) {
+    return mesespedidos;
+  }
+  else {
+    return "No se encontraron los meses pedidos";
+  }
 }
+
+var mesesiniciales = ["Junio", "Marzo", "Mayo", "Enero", "Noviembre"];
+var mesesfiltrados = mesesDelAño(mesesiniciales);
+
+console.log(mesesfiltrados);
 
 module.exports = mesesDelAño;
