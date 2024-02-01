d9fd3a4316ca64055394f5115a0df45178a34f1b
unknown <rfloresh92@gmail.com>
2024-01-31 20:47:56 -0500

    Arrays 15

--- M07 Arrays/ejercicios/15.js
+++ M07 Arrays/ejercicios/15.js
@@ -2,6 +2,23 @@ function todosIguales(array) {
   // Si todos los elementos del arreglo son iguales, retornar true.
   // Caso contrario, retornar false.
   // Tu código:
+  
+  for (let i = 1; i < array.length; i++) {
+    if(array[i] !== array[0]) {
+      return false;
+    }
+
+  }
+  
+  return true;
+
 }
+
+console.log(todosIguales([1, 1, 1]));
+console.log(todosIguales([1, 2, 3]));
+
+
+
+
 
 module.exports = todosIguales;
