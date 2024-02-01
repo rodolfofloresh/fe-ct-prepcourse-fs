d3659491d1338150550f68a9feb5cb5afecd4bbd
unknown <rfloresh92@gmail.com>
2024-01-31 20:08:54 -0500

    Arrays 14

--- M07 Arrays/ejercicios/14.js
+++ M07 Arrays/ejercicios/14.js
@@ -1,8 +1,26 @@
-function multiplicarArgumentos() {
+function multiplicarArgumentos(arguments) {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto.
   // Si no se pasan argumentos retorna 0. Si se pasa un argumento, simplemente retórnalo.
   // [PISTA]: "arguments" es un arreglo.
   // Tu código:
+  var multiplicacion = 1;
+  if ( arguments.length === 0){
+    return 0;
+  }
+  else if(arguments.length === 1) {
+    return arguments;
+  }
+  for (let i = 0; i < arguments.length; i++) {
+    multiplicacion = multiplicacion * arguments[i];
+  }
+
+  return multiplicacion;
+
 }
+
+var miarray = [2,2,2];
+var resultado = multiplicarArgumentos(miarray);
+console.log(resultado);
+
 
 module.exports = multiplicarArgumentos;
