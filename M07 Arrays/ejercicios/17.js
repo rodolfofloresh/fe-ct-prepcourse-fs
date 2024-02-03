e9c5cd359176b94a507c1db5dbe604af1f4ca36f
unknown <rfloresh92@gmail.com>
2024-02-03 16:58:12 -0500

    Arrays 17

--- M07 Arrays/ejercicios/17.js
+++ M07 Arrays/ejercicios/17.js
@@ -2,6 +2,18 @@ function tablaDelSeis() {
   // Devuelve un arreglo con los resultados de la tabla de multiplicar del 6 en orden creciente.
   // Desde 0 al 60.
   // Tu código:
+
+  var tabla = [];
+
+  for(let i = 0; i <=10; i++) {
+    tabla.push(i * 6);
+  }
+
+  return tabla;
+
 }
+
+var tabla6 = tablaDelSeis();
+console.log(tabla6);
 
 module.exports = tablaDelSeis;
