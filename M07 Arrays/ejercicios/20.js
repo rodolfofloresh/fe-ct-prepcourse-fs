dba8f8d8f3d8271ab72dae75f4e22d60ab21cdb7
unknown <rfloresh92@gmail.com>
2024-02-03 17:54:07 -0500

    Arrays

--- M07 Arrays/ejercicios/20.js
+++ M07 Arrays/ejercicios/20.js
@@ -3,6 +3,17 @@ function contarParesConContinue(numeros) {
   // Devuelve la cantidad de números pares que hay en el array.
   // Evita los impares utilizando continue.
   // Tu código:
+  var par = numeros.filter(function(numeros) {
+    return numeros % 2 === 0;
+  })
+  
+  return par.length;
+  
+
 }
+
+var miarreglo = [3, 4, 5, 6, 7, 8, 9];
+var pares = contarParesConContinue(miarreglo);
+console.log(pares);
 
 module.exports = contarParesConContinue;
