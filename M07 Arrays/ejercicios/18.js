6972965fe59552d8cafea8720b828612aef46924
unknown <rfloresh92@gmail.com>
2024-02-03 17:16:19 -0500

    Arrays 18

--- M07 Arrays/ejercicios/18.js
+++ M07 Arrays/ejercicios/18.js
@@ -4,7 +4,24 @@ function breakStatement(num) {
   // Si en algún momento el valor de la suma y la cantidad de iteraciones coinciden, debe interrumpirse
   // la ejecución y retornar el string: "Se interrumpió la ejecución".
   // PISTA: Utiliza el statement 'break'.
-  // Tu código:
+  // Tu código: 
+  var arregloaumentado = [];
+  for(let i = 0; i < 10; i++) {
+    num = num + 2;
+    arregloaumentado.push(num);
+
+    if(num === i) {
+      return "Se interumpe la ejecucion";
+    }
+
+  }
+
+  return arregloaumentado;
+
+
 }
+
+var resultado = breakStatement(4,);
+console.log(resultado);
 
 module.exports = breakStatement;
