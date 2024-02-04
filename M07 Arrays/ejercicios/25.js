07e70f6af4997064b8965d2fe5de97a27fbaf036
unknown <rfloresh92@gmail.com>
2024-02-03 20:13:44 -0500

    Arrays 26

--- M07 Arrays/ejercicios/25.js
+++ M07 Arrays/ejercicios/25.js
@@ -2,6 +2,16 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Agrega el "elemento" al final del arreglo recibido.
   // Retorna el arreglo.
   // Tu código:
-}
+  array.push(elemento);
+  return array;
+ 
+  
+
+} 
+var miarray = [1, 2, 3];
+var elementoagregado = 4;
+var resultado = agregarItemAlFinalDelArray(miarray, elementoagregado);
+console.log(resultado);
+
 
 module.exports = agregarItemAlFinalDelArray;
--- M07 Arrays/ejercicios/26.js
+++ M07 Arrays/ejercicios/26.js
@@ -2,6 +2,13 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // Agrega el "elemento" al comienzo del arreglo recibido.
   // Retorna el arreglo.
   // Tu código:
+  array.unshift(elemento);
+  return array;
 }
+
+var miarray = [4, 77, 30];
+var elementoagregado = 18;
+var resultado = agregarItemAlComienzoDelArray(miarray, elementoagregado);
+console.log(resultado);
 
 module.exports = agregarItemAlComienzoDelArray;
