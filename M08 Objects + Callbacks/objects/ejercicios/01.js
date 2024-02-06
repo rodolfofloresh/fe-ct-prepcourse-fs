98a5627b4dcc47d4697f79a1b95e5efc0cea1fec
unknown <rfloresh92@gmail.com>
2024-02-06 13:38:51 -0500

    objetos 02

--- M08 Objects + Callbacks/objects/ejercicios/01.js
+++ M08 Objects + Callbacks/objects/ejercicios/01.js
@@ -2,6 +2,22 @@ const obtenerValorPropiedad = (objeto, propiedad) => {
   // Recibe un objeto y el nombre de una propiedad.
   // Devuelve el valor de esta propiedad.
   // Tu código:
+ 
+  return objeto[propiedad];
+  
+
 };
+
+var miObjeto = {
+  nombre: "Rodolfo",
+  edad: 32,
+  pais: "Peru",
+
+};
+
+
+console.log(obtenerValorPropiedad(miObjeto, "edad"));
+
+
 
 module.exports = obtenerValorPropiedad;
--- M08 Objects + Callbacks/objects/ejercicios/02.js
+++ M08 Objects + Callbacks/objects/ejercicios/02.js
@@ -2,6 +2,21 @@ function actualizarValorPropiedad(objeto, propiedad, valor) {
   // Actualiza el valor de la propiedad del objeto recibidos en la función.
   // Retorna el objeto actualizado.
   // Tu código:
+
+  objeto[propiedad] = valor;
+  return objeto;
+
 }
+
+var miObjeto = {
+  animal: "Perro",
+  raza: "Bulldog",
+  edad: 3,
+}
+
+console.log("Objeto original:", miObjeto);
+miObjeto = actualizarValorPropiedad(miObjeto, "edad", 7);
+console.log("Objeto actualizado", miObjeto);
+
 
 module.exports = actualizarValorPropiedad;
